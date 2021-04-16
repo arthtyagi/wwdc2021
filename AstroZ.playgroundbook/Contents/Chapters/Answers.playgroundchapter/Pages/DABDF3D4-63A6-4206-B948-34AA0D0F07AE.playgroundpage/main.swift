8fd4cbf12e9c7382942b2954966b835830b6767d
Arth Tyagi <arthtyagi7@gmail.com>
2021-04-16 18:02:39 +0530

    terrible project built minus everything I was planning about the rocket simulation, good thing is - iOS app plan is ONNNN

---
+++ AstroZ.playgroundbook/Contents/Chapters/Answers.playgroundchapter/Pages/DABDF3D4-63A6-4206-B948-34AA0D0F07AE.playgroundpage/main.swift
@@ -0,0 +1,23 @@
+//#-hidden-code
+//
+//  main.swift
+//  
+//  Copyright © 2016-2020 Apple Inc. All rights reserved.
+//
+//#-end-hidden-code
+//#-code-completion(identifier, hide, setupLiveView())
+//#-hidden-code
+import Foundation
+import UIKit
+//#-end-hidden-code
+/**
+ # Practical Rocketry 
+*/
+
+show ("How do Rockets even work?")
+
+show("Rocket Engines and Their Propellant Most rockets today operate with either solid or liquid propellants. The word propellant does not mean simply fuel, as you might think; it means both fuel and oxidizer. The fuel is the chemical rockets burn but, for burning to take place, an oxidizer (oxygen) must be present. Jet engines draw oxygen into their engines from the surrounding air. Rockets do not have the luxury that jet planes have; they must carry oxygen with them into space, where there is no air. Solid rocket propellants, which are dry to the touch, contain both the fuel and oxidizer combined together in the chemical itself. Usually the fuel is a mixture of hydrogen compounds and carbon and the oxidizer is made up of oxygen compounds. Liquid propellants, which are often gases that have been chilled until they turn into liquids, are kept in separate containers, one for the fuel and the other for the oxidizer. Then, when the engine fires, the fuel and oxidizer are mixed together in the engine.")
+
+show("A solid-propellant rocket has the simplest form of engine. It has a nozzle, a case, insulation, propellant, and an igniter. The case of the engine is usually a relatively thin metal that is lined with insulation to keep the propellant from burning through. The propellant itself is packed inside the insulation layer. Many solid-propellant rocket engines feature a hollow core that runs through the propellant. Rockets that do not have the hollow core must be ignited at the lower end of the propellants and burning proceeds gradually from one end of the rocket to the other. In all cases, only the surface of the propellant burns. However, to get higher thrust, the hollow core is used. This increases the surface of the propellants available for burning. The propellants burn from the inside out at a much higher rate, and the gases produced escape the engine at much higher speeds. This gives a greater thrust. Some propellant cores are star shaped to increase the burning surface even more.To fire solid propellants, many kinds of igniters can be used. Fire-arrows were ignited by fuses, but sometimes these ignited too quickly and burned the rocketeer. A far safer and more reliable form of ignition used today is one that employs electricity. An electric current, coming through wires from some distance away, heats up a special wire inside the rocket. The wire raises the temperature of the propellant it is in contact with to the combustion point. Other igniters are more advanced than the hot wire device. Some are encased in a chemical that ignites first, which then ignites the propellants. Still other igniters, especially those for large rockets, are rocket engines themselves. The small engine inside the hollow core blasts a stream of flames and hot gas down from the top of the core and ignites the entire surface area of the propellants in a fraction of a second. The nozzle in a solid-propellant engine is an opening at the back of the rocket that permits the hot expanding gases to escape. The narrow part of the nozzle is the throat. Just beyond the throat is the exit cone. The purpose of the nozzle is to increase the acceleration of the gases as they leave the rocket and thereby maximize the thrust. It does this by cutting down the opening through which the gases can escape. To see how this works, you can experiment with a garden hose that has a spray nozzle attachment. This kind of nozzle does not have an exit cone, but that does not matter in the experiment. The important point about the nozzle is that the size of the opening can be varied. Start with the opening at its widest point. Watch how far the water squirts and feel the thrust produced by the departing water. Now reduce the diameter of the opening, and again note the distance the water squirts and feel the thrust. Rocket nozzles work the same way. As with the inside of the rocket case, insulation is needed to protect the nozzle from the hot gases. The usual insulation is one that gradually erodes as the gas passes through. Small pieces of the insulation get very hot and break away from the nozzle. As they are blown away, heat is carried away with them.")
+
+show (#imageLiteral(resourceName: "axes.gif"))
---
+++ AstroZ.playgroundbook/Contents/Chapters/Answers.playgroundchapter/Pages/Text.playgroundpage/main.swift
@@ -0,0 +1,19 @@
+//#-hidden-code
+//
+//  main.swift
+//  
+//  Copyright © 2016-2020 Apple Inc. All rights reserved.
+//
+//#-end-hidden-code
+//#-code-completion(identifier, hide, setupLiveView())
+//#-hidden-code
+import Foundation
+//#-end-hidden-code
+//#-editable-code Tap to enter code
+show("What’s your name?")
+
+let name = ask("Name")
+
+show("Hi " + name + ", " + "you have been invited to the AstroZ team! Our mission is to make great rockets and your task is to ensure that our rockets work perfectly!")
+
+//#-end-editable-code
